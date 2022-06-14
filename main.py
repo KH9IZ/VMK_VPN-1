d176b0b3d417c015e11e161c585234ee29fde905
Knyazev Egor <knyazevegor01@mail.ru>
2022-06-15 01:22:26 +0300

    Payment minimal

--- main.py
+++ main.py
@@ -39,11 +39,45 @@ def send_welcome(message):
     """
     Handler for /start command
     """
-    markup = gen_markup({"config":  _("Get your config!")}, 1)
+    markup = gen_markup({"pay":  _("Make payment")}, 1)
     bot.send_message(chat_id=message.chat.id,
                      text=_(
                          "Welcome to the CMC MSU bot for fast and secure VPN connection!"),
                      reply_markup=markup)
+
+
+@bot.callback_query_handler(func=lambda call: call.data == "pay")
+def payment(call):
+    bot.send_invoice(
+        chat_id=call.message.chat.id,
+        title=_("Month of VPN"),
+        description=_("Fast and sequre connection to virtual private network specially for MSU students!"),
+        invoice_payload=call.message.chat.id,
+        provider_token=os.environ["VPN_PAYMENT_PROVIDER_TOKEN"],
+        currency="RUB",  # TODO тут бы повыносить некоторые вещи в конфиг какой-нибудь чтоли
+        prices=[
+            telebot.types.LabeledPrice(label=_("1 month"), amount=7000),
+        ],
+        photo_url="https://i.ibb.co/bbPtqjR/Inkedeeeecaf1d4cc8858fa21567085a3ab30-LI.jpg",
+        start_parameter=call.message.chat.id
+    )
+    bot.answer_callback_query(call.id)
+    print("payment:", call.__dict__)
+    # Осторожно, нужно проверить, как это будет работать если пользователь перешлет оплату в другой чат
+
+
+@bot.pre_checkout_query_handler(func=lambda pc_query: True)
+def pre_checkout_answer(pc_query):
+    bot.answer_pre_checkout_query(pc_query.id, ok=True)
+    print("pre_checkout_answer:", pc_query.__dict__)
+
+
+@bot.message_handler(content_types=['successful_payment'])
+def successful_payment(msg):
+    # TODO Ну раз оплатил можно и в БД добавить
+    markup = gen_markup({"config":  _("Get your config!")}, 1)
+    bot.send_message(msg.chat.id, _("Thank you for choosing our VPN!"), reply_markup=markup)
+    print("successful_payment:", msg.__dict__)
 
 
 @bot.callback_query_handler(func=lambda call: call.data == "config")
